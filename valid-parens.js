3a2849312bb9c151030911f4cf84523f0e873211
Rachel <racheljay42@gmail.com>
2022-07-26 16:34:02 -0400

    parens refactor

--- valid-parens.js
+++ valid-parens.js
@@ -4,38 +4,25 @@
  */
 
 const braceRelations = {
-    "{": "}",
-    "(": ")",
-    "[": "]",
     "}": "{",
     ")": "(",
     "]": "["
 }
- var validParens = function(s) {
-    
-    // let parensOpen = false
-    // let bracketOpen = false
-    // let curlyOpen = false
+const openers = new Set(Object.values(braceRelations))
+const closers = new Set(Object.keys(braceRelations))
+
+ var isValid = function(s) {
 
     let bracketTally = []
-    
-    const isOpen = str => {
-        if (str === "(" || str === "{" || str === "[") return true
-        return false
-    }
-
-    const isClosed = str => {
-        if (str === ")" || str === "}" || str === "]") return true
-    }
     
     for(let i = 0; i < s.length; i++) {
         item = s[i]
 
-        if (isOpen(item)) {
+        if (openers.has(item)) {
             bracketTally.unshift(item)
         }
 
-        if(isClosed(item)) {
+        if(closers.has(item)) {
             if(bracketTally[0] === braceRelations[item]) {
                 bracketTally.shift()
             } else if(bracketTally[0] !== braceRelations[item]) {
@@ -44,9 +31,8 @@ const braceRelations = {
         }
     }
     if(bracketTally.length > 0) return false
-    console.log(bracketTally)
     return true
     
 };
 
-console.log(validParens("([)]"))
+console.log(isValid("()[{}"))
