54f8d7c2d418729e1f4c4be935b5f4a1a33cf6c3
Rachel <racheljay42@gmail.com>
2022-08-09 18:37:43 -0400

    working solution for merging linked list

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -12,60 +12,60 @@
  */
  var mergeTwoLists = function(list1, list2) {
     // edge cases
-    if (list1 === null && list2 === null) return null
-    if (list1 === null && list2 !== null) return list2
-    if (list2 === null && list1 !== null) return list1
+    if(list1 === null && list2 === null) return null
+    if(list1 === null && list2 !== null) return list2
+    if(list1 !== null && list2 === null) return list1
     
-    /* 
-        make sure our first current iterative node is
-        going to be smaller than the head of the one we're comparing to
-    */
-    let iterative, comparative
-    if(list1.val > list2.val) {
-        iterative = list2
-        comparative = list1
-    }
-    if(list1.val <= list2.val) {
-        iterative = list1
-        comparative = list2
-    }
+    console.log(list1.val)
     
-    let current = iterative
-    // let compare = comparative
-    
-    // console.log("next next val", iterative.next.next.val)
-    
-    // while (iterative.next !== null) {
-        let oldNext = iterative.next
-        // console.log(iterative.val, comparative.val)
+    // insertion method
+    const insertInOrder = (data) => {
+        let current = list1
         
-        if(iterative.val >= comparative.val) {
-            iterative.next = comparative.val
-            iterative.next.next = oldNext
-            // break
+        while(current.next !== null) {
+            // console.log("check check",current.val)
+            if(current.val <= data && current.next.val >= data) {
+                let oldNext = current.next
+                current.next = new ListNode(data)
+                current.next.next = oldNext
+                return
+            }
+            current = current.next
         }
-        // iterative = iterative.next
-  //  } 
-    console.log(iterative.val)
-    
-    console.log(iterative)
-//     let current = list1
-//     let list2head = list2.val
-//     while (current.next !== null) {
-//         let oldnext = current.next
-//         console.log(current.val)
+        // assuming that current.next is null
+        // case for if the second list of numbers does not blend with the first list
+        if(current.next === null && current.data !== null) {
+            if(data >= current.val) {
+                current.next = new ListNode(data)
+                return
+            }
+        }
+        if(list1.val <= data) {
+            list1.next = new ListNode(data)
+            return
+        } else {
+            let oldHead = list1
+            let newHead = new ListNode(data)
+            newHead.next = oldHead
+            list1 = newHead
+            return
+        }
+    }
+
+    // merge method
+    const mergeList = () => {
+        let current = list2
+        console.log(current)
         
-//         if(list2head )
-        
-//         current = current.next
-//     }
-//     console.log(current.val)
-    
-//     let data2 = list2
-//     while (data2.next !== null) {
-//         console.log(data2.val)
-//         data2 = data2.next
-//     }
-//     console.log(data2.val)
+        while(current.next !== null) {
+            console.log(current.val)
+            insertInOrder(current.val)
+            current = current.next
+        }
+        insertInOrder(current.val)
+    }
+    mergeList()
+    console.log(list1)
+    return list1
     
 };
