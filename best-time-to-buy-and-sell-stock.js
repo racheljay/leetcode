7a0d7475760cc9d0b5d6c232d7aecc6b5ca8b617
Rachel <racheljay42@gmail.com>
2022-08-10 17:13:54 -0400

    all stock cases passing

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -4,8 +4,8 @@
  */
 var maxProfit = function (prices) {
 
-    let lowest = -1
-    let highest = 0
+    let lowest = Number.MAX_SAFE_INTEGER
+    let highest = Number.MIN_SAFE_INTEGER
     let diff = 0
     let profit = 0
     let lowestIndex = 0
@@ -13,14 +13,10 @@ var maxProfit = function (prices) {
     for (let i = 0; i < prices.length; i++) {
         let price = prices[i]
 
-        // find optimal lowest number
-        if (lowest < 0) {
-            lowest = price
-            lowestIndex = i
-        }
         if (price <= lowest) {
             lowest = price
             lowestIndex = i
+            highest = Number.MIN_SAFE_INTEGER
         }
 
         // find optimal highest
@@ -44,3 +40,4 @@ maxProfit([7, 1, 5, 3, 6, 4]) // should return 6 - 1
 maxProfit([7, 6, 4, 3, 1]) // should return 0
 maxProfit([2, 4, 1]) // should return 4 - 2 = 2
 maxProfit([2, 1, 2, 1, 0, 1, 2]) // should return 2 - 0
+maxProfit([3,3,5,0,0,3,1,4]) // should return 4 - 0
