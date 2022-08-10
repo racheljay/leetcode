1dcf65d52afc9e77e8b433d33c2bff48a0063b76
Rachel <racheljay42@gmail.com>
2022-08-10 15:46:24 -0400

    issue with implementation

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -15,21 +15,27 @@ var maxProfit = function (prices) {
             lowest = price
         }
     }
-    console.log(lowest)
+    // console.log(lowest)
     let lowestIndex = prices.indexOf(lowest)
+    if(lowestIndex === prices.length -1) {
+        console.log("no dice", 0)
+        return 0
+    }
 
     const remainder = prices.slice(lowestIndex + 1, prices.length)
-    console.log(remainder)
+    // console.log(remainder)
 
     let highest = 0
 
     for (let item of remainder) {
         if(item > highest) highest = item
     }
-    console.log(highest)
+    // console.log(highest)
     console.log(highest - lowest)
     return highest - lowest
 
 };
 
-maxProfit([7, 1, 5, 3, 6, 4])
+// maxProfit([7, 1, 5, 3, 6, 4]) // should return 6 - 1
+// maxProfit([7,6,4,3,1]) // should return 0
+maxProfit([2,4,1]) // should return 4 - 2 = 2
