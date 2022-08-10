914b432c397f8c5546cffc217861506e6926940e
Rachel <racheljay42@gmail.com>
2022-08-10 16:42:42 -0400

    some cases working

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -5,37 +5,37 @@
 var maxProfit = function (prices) {
 
     let lowest = -1
+    let highest = 0
+    let diff = 0
+    let profit = 0
+    let lowestIndex = 0
 
-    for (let price of prices) {
-        // console.log(price)
-        if (lowest < 0) {
-            lowest = price
-            continue
-        } else if (lowest > price) {
-            lowest = price
+    for (let i = 0; i < prices.length; i++) {
+        let price = prices[i]
+
+        // find optimal lowest number
+        if (lowest < 0) lowest = price
+        if (price < lowest) lowest = price
+
+        // let lowestIndex = prices.indexOf(lowest)
+        // let highestIndex = prices.indexOf(highest)
+
+        // find optimal highest
+        if (i !== 0) {
+            if (highest < price && i > lowestIndex) {
+                highest = price
+                diff = highest - lowest
+                //find optimal profit
+                if (diff > profit) profit = diff
+            }
         }
     }
-    // console.log(lowest)
-    let lowestIndex = prices.indexOf(lowest)
-    if(lowestIndex === prices.length -1) {
-        console.log("no dice", 0)
-        return 0
-    }
-
-    const remainder = prices.slice(lowestIndex + 1, prices.length)
-    // console.log(remainder)
-
-    let highest = 0
-
-    for (let item of remainder) {
-        if(item > highest) highest = item
-    }
-    // console.log(highest)
-    console.log(highest - lowest)
-    return highest - lowest
+    console.log(profit)
+    return profit
 
 };
 
 // maxProfit([7, 1, 5, 3, 6, 4]) // should return 6 - 1
-// maxProfit([7,6,4,3,1]) // should return 0
-maxProfit([2,4,1]) // should return 4 - 2 = 2
+// maxProfit([7, 6, 4, 3, 1]) // should return 0
+// maxProfit([2, 4, 1]) // should return 4 - 2 = 2
+maxProfit([2, 1, 2, 1, 0, 1, 2]) // should return 2 - 0
