34d3f7a319bd0bcb5c16fac006ecbd305b1e5b09
Rachel <racheljay42@gmail.com>
2022-08-09 20:19:44 -0400

    stonks

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -2,6 +2,34 @@
  * @param {number[]} prices
  * @return {number}
  */
- var maxProfit = function(prices) {
-    
+var maxProfit = function (prices) {
+
+    let lowest = -1
+
+    for (let price of prices) {
+        // console.log(price)
+        if (lowest < 0) {
+            lowest = price
+            continue
+        } else if (lowest > price) {
+            lowest = price
+        }
+    }
+    console.log(lowest)
+    let lowestIndex = prices.indexOf(lowest)
+
+    const remainder = prices.slice(lowestIndex + 1, prices.length)
+    console.log(remainder)
+
+    let highest = 0
+
+    for (let item of remainder) {
+        if(item > highest) highest = item
+    }
+    console.log(highest)
+    console.log(highest - lowest)
+    return highest - lowest
+
 };
+
+maxProfit([7, 1, 5, 3, 6, 4])
