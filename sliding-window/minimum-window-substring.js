97e16aab8d1c158de8a19255632f58ae9c6e3abc
Rachel <racheljay42@gmail.com>
2022-12-22 18:36:19 -0500

    working for some test cases

--- sliding-window/minimum-window-substring.js
+++ sliding-window/minimum-window-substring.js
@@ -4,6 +4,11 @@
  * @return {string}
  */
 var minWindow = function (s, t) {
+    if (t.length > s.length) {
+        console.log("aint gonna fit")
+        return ""
+    }
+
     const mustHaves = {}
     for (let i = 0; i < t.length; i++) {
         if (t[i] in mustHaves === false) {
@@ -17,13 +22,13 @@ var minWindow = function (s, t) {
     let numOfLetters = 0
     let start = 0
     let end = 0
-    let minLength = Number.MAX_SAFE_INTEGER
+    let minLength = [Number.MAX_SAFE_INTEGER, start, end]
 
     while (end < s.length) {
         const letter = s[end]
 
         if (letter in mustHaves) {
-            console.log({letter})
+            console.log({ letter })
             if (letter in current === false) {
                 current[letter] = 0
             }
@@ -33,32 +38,61 @@ var minWindow = function (s, t) {
             current[letter]++
         }
 
-        // TODO figure out how to shrink the window in a reasonable way while correct amount of letters are present
-        // while(numOfLetters > mustHaveVariety) {
-        //     if()
-        // }
+        while (numOfLetters >= mustHaveVariety) {
+            if (numOfLetters === mustHaveVariety) {
 
-        if(numOfLetters === mustHaveVariety) {
-            minLength = Math.min(minLength, (end - start + 1))
-            console.log({start}, {end})
-            console.log({numOfLetters}, {minLength})
-            // break
-            if(s[start] in mustHaves) {
-                current[s[start]]--
-                start++
-                numOfLetters--
+                if (end - start + 1 < minLength[0]) {
+                    minLength = [end - start + 1, start, end]
+                }
+
+                console.log({ start }, s[start], { end }, s[end])
+                console.log({ numOfLetters }, { minLength })
+
             }
+            if (s[start] in mustHaves) {
+                if (current[s[start]] > 0) {
+                    current[s[start]]--
+                }
+                if (current[s[start]] <= 0)
+                    numOfLetters--
+            }
+            start++
         }
-
         end++
     }
 
-
-
     console.log(current)
+    if (Object.keys(current).length === 0) {
+        console.log("no match")
+        return ""
+    }
+    const minString = s.slice(minLength[1], minLength[2] + 1)
+    console.log("SOLUTION:", { minString })
+    return minString
+
 };
 
 const s1 = "ADOBECODEBANC"
 const t1 = "ABC"
 
-minWindow(s1, t1)
+// minWindow(s1, t1)
+
+const s2 = "a"
+const t2 = "a"
+
+// minWindow(s2, t2)
+
+const s3 = "a"
+const t3 = "aa"
+
+// minWindow(s3, t3)
+
+const s4 = "a"
+const t4 = "b"
+
+// minWindow(s4, t4)
+
+const s5 = "aa"
+const t5 = "aa"
+
+minWindow(s5, t5)
