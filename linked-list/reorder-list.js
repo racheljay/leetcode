c0087afeb24cb3a2784af92898358e6e783f1b17
Rachel <racheljay42@gmail.com>
2023-01-17 18:35:21 -0800

    wip: reverse method messing up original list

--- linked-list/reorder-list.js
+++ linked-list/reorder-list.js
@@ -9,6 +9,73 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-var reorderList = function(head) {
+
+/* ****** Take this out before submitting to leetocde ******** */
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+/********************************************* */
+
+/* Playground to get back on my feet*/
+
+const list = new ListNode(1)
+list.next = new ListNode(2)
+list.next.next = new ListNode(3)
+list.next.next.next = new ListNode(4)
+
+// console.log(list)
+
+const printList = ll => {
+    let node = ll
+    const nodeArr = []
+
+    if (node === null) {
+        console.log("empty list")
+    }
+    while (node !== null) {
+        nodeArr.push(node.val)
+        node = node.next
+    }
+    console.log(nodeArr)
+}
+
+
+const reverseList = ll => {
+    let current = ll
+    let previous = null
+    let after = ll.next
+
+    while (after !== null) {
+        current.next = previous
+        previous = current
+        current = after
+        after = current.next
+    }
+    current.next = previous
+    return current
+
+}
+
+// const reversed = reverseList(list)
+// printList(reversed)
+
+/* end playground */
+
+var reorderList = function (head) {
+    let current = head
+    // const reversed = reverseList(current)
+    printList(current)
+
+// let oldNext = current.next
+// current.next = e
     
+
 };
+
+
+/* Test Cases */
+
+reorderList(list)
