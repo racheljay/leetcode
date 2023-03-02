dbfeb04882906c4e085fe45f3d8a7bf8656396f5
Rachel <racheljay42@gmail.com>
2023-03-01 19:01:41 -0800

    search in sorted array

---
+++ binary-search/search-in-sorted-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let left = 0
+    let right = nums.length - 1
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+
+        if (target === nums[mid]) {
+            return mid
+        }
+        
+        if (nums[left] <= nums[mid]) {
+            if (target > nums[mid] || target < nums[left]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        } else {
+            if (target < nums[mid] || target > nums[right]) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+    }
+    return -1
+};
